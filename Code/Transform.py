75943a84b3c1f0a19e93cddea18217210fa990a7
JPdeKlerk <91538962+JPdeKlerk@users.noreply.github.com>
2024-03-06 08:03:50 +0200

    Update Transform.py

--- Code/Transform.py
+++ Code/Transform.py
@@ -9,7 +9,7 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # Define the schema for the JSON data
-schema3= StructType([
+schema = StructType([
     StructField("url", StringType(), True),
     StructField("id", IntegerType(), True),
     StructField("node_id", StringType(), True),
@@ -43,7 +43,7 @@ schema3= StructType([
 
 def transform_and_aggregate(json_path):
     # Read the JSON data and perform transformation
-    transformed_df = spark.read.json(json_path, schema3).groupBy(
+    transformed_df = spark.read.json(json_path, schema).groupBy(
         split(col("base.repo.full_name"), "/")[0].alias("organization_name"),  # Splitting organization_name by "/"
         col("base.repo.id").alias("repository_id"),
         col("base.repo.name").alias("repository_name"),
